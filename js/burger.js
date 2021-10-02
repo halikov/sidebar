c6c888f96bb9d2a7e3ddf5a54ffb7dae94b706f8
Mikail Halikov <mkhalikov@gmail.com>
2021-10-03 00:07:39 +0300

    добавляет картинки, шрифты, стили, разметку

---
+++ js/burger.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let burger = document.querySelector('.sidebar__burger');
+let sidebar = document.querySelector('.sidebar');
+let themeDot = document.querySelector('.theme__input');
+let label = document.querySelector('.theme');
+let pageBody = document.querySelector('body');
+
+burger.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  sidebar.classList.toggle('active');
+});
+
+function themeChange () {
+  
+  label.addEventListener('click', () => {
+    if(themeDot.checked) {
+      pageBody.classList.add('dark-mode');
+    } else {
+      pageBody.classList.remove('dark-mode');
+    }
+  });
+
+};
+
+label.addEventListener('click', () => {
+  themeChange();
+})
+
